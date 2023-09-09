93fcdf03de49ec2daeb534116fac32c23bb9ddf9
neeel101 <neeleshvinchoorkar@gmail.com>
2023-09-09 14:35:55 +0530

    another

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const path = require("path");
 const { connectToMongoDb } = require("./connect");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 8001  ; 
+const PORT = process.env.PORT || 8001;
 const app = express();
 //mongo DB connection
-connectToMongoDb(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() =>
-  console.log("MongoDb connected ! ")
-).catch(err => console.log(err))
+try {
+  connectToMongoDb(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log("MongoDb connected ! "));
+} catch (err) {
+  (err) => console.log(err);
+}
 
 //listen
 app.listen(PORT, () => console.log("Express Server started on PORT", PORT));
@@ -16,9 +21,7 @@ app.listen(PORT, () => console.log("Express Server started on PORT", PORT));
 //routers
 const UrlRouter = require("./routes/url");
 const staticRouter = require("./staticRoute/staticRoute");
-const {userRouter} = require("./routes/user")
-
-
+const { userRouter } = require("./routes/user");
 
 //route handling (middlewares)
 
@@ -26,12 +29,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //routing (static and dynamic)
-app.use("/url", UrlRouter); 
+app.use("/url", UrlRouter);
 app.use("/", staticRouter);
 app.use("/user", userRouter);
 
 //server side rendering components
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-
-
